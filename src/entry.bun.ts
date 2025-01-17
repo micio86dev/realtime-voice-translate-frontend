cdde58ac8105193fad64c62169eb6d959af991ca
Alessandro Micelli <micio86dev@gmail.com>
2025-01-17 11:45:50 +0000

    fix: bun 1

--- src/entry.bun.ts
+++ src/entry.bun.ts
@@ -21,10 +21,10 @@ const { router, notFound, staticFile } = createQwikCity({
 });
 
 // Allow for dynamic port
-const port = Number(Bun.env.PORT ?? 3012);
+const port = Number(Bun.env.PORT ?? 3000);
 
 // eslint-disable-next-line no-console
-console.log(`Server started: http://localhost:${ port }/`);
+console.log(`Server started: http://localhost:${port}/`);
 
 Bun.serve({
   async fetch(request: Request) {
