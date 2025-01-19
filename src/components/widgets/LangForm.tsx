34c99bd8282bf01199a128878f894f22988fc36f
Alessandro Micelli <micio86dev@gmail.com>
2025-01-19 13:18:40 +0000

    fix

--- src/components/widgets/LangForm.tsx
+++ src/components/widgets/LangForm.tsx
@@ -22,11 +22,13 @@ export default component$(() => {
     sendingMessage: false,
     artyom: null as any,
     voices: [],
+    userId: '',
     selectedVoice: 'Alice',
     receivedMessage: '',
     sourceLang: getLocale(), // Default from the browser
-    targetLang: 'es',
-    onlineUsers: [],
+    targetLang: '',
+    onlineUsers: [] as any,
+    selectedUser: null as any,
     supportedLangs: [
       { name: $localize`English`, id: 'en', lang: 'en-GB' },
       { name: $localize`Spanish`, id: 'es', lang: 'es-ES' },
@@ -50,6 +52,7 @@ export default component$(() => {
 
   // Set first of available voices
   const setVoices = $(() => {
+    console.log(store.artyom.getVoices(), 'C')
     const voices = store.artyom.getVoices()
       .filter((voice: any) => voice.lang.includes(store.targetLang) && voice.localService)
       .map((voice: any) => { return { name: voice.name, id: voice.name } });
@@ -110,13 +113,19 @@ export default component$(() => {
     });
   });
 
-  const artyomInit = $(() => {
+  const artyomInit = $(async () => {
+    const ArtyomModule = await import('artyom.js');
+    const Artyom = ArtyomModule.default;
+    const artyomInstance = new Artyom();
+
+    // Inizializza Artyom lato client
+    store.artyom = noSerialize(artyomInstance);
     store.artyom.initialize({
       lang: getFullLocale(),
       continuous: true,
       soundex: true,
       listen: true,
-      debug: true,
+      debug: false,
       mode: 'remote',
       volume: 1,
       speed: 1,
@@ -124,65 +133,90 @@ export default component$(() => {
       setVoices();
 
       store.artyom.remoteProcessorService((phrase: any) => {
-        store.speaking = true;
+        if (!store.selectedUser) {
+          alert($localize`Please, choose a user`);
+        } else {
+          store.speaking = true;
 
-        if (phrase.isFinal) {
-          store.message = phrase.text;
-          if (phrase.text !== '' && store.channelName !== '') {
-            // Send message to translate service
-            sendMessage();
+          if (phrase.isFinal) {
+            store.message = phrase.text;
+            if (phrase.text !== '' && store.channelName !== '') {
+              // Send message to translate service
+              sendMessage();
+            }
+            store.speaking = false;
           }
-          store.speaking = false;
         }
       });
 
       store.listening = true;
     }).catch((err: any) => {
       console.error(err);
+      alert($localize`Error initializing your microphone`);
     });
   });
 
-  useOnDocument("qinit", $(async () => {
-    const ArtyomModule = await import('artyom.js');
-    const Artyom = ArtyomModule.default;
-    const artyomInstance = new Artyom();
+  const addMember = $((member: any) => {
+    store.onlineUsers.push(member);
+  });
 
-    // Inizializza Artyom lato client
-    store.artyom = noSerialize(artyomInstance);
-    artyomInit();
+  const removeMember = $((member: any) => {
+    store.onlineUsers = store.onlineUsers.filter((m: any) => m.id !== member.id);
+  });
 
-    const userId = crypto.randomUUID();
-    store.channelName = `user-${ userId }`;
+  const selectUser = $((event: Event) => {
+    const target = event.target as HTMLSelectElement;
+    store.selectedUser = target.value;
+  });
 
+  const initPresenceChannel = $(() => {
+    const presenceChannel = store.pusher.subscribe('presence-chat');
+
+    // When a user subscribes
+    presenceChannel.bind('pusher:subscription_succeeded', (members: any) => {
+      const membersIds = Object.keys(members.members);
+
+      membersIds.forEach((id: string) => {
+        if (id !== store.userId) {
+          const member = members.members[id];
+          console.log(`User subscribed: ${ id } - ${ member.name }`);
+          addMember({
+            id,
+            name: member.name,
+          });
+        }
+      });
+    });
+
+    // When a user joins
+    presenceChannel.bind('pusher:member_added', (member: any) => {
+      console.log(`${ member } joined`);
+      addMember(member);
+    });
+
+    // When a user leaves
+    presenceChannel.bind('pusher:member_removed', (member: any) => {
+      console.log(`${ member } leaved`);
+      removeMember(member);
+    });
+  });
+
+  const initPusher = $(() => {
+    store.userId = crypto.randomUUID();
+    store.channelName = `user-${ store.userId }`;
+
+    // Init pusher
     import('pusher-js').then(PusherModule => {
       store.pusher = noSerialize(new PusherModule.default('27991ede2e5f0b8d86d9', {
         cluster: 'eu',
         authEndpoint: `${ backendUrl }/pusher/auth`,
         auth: {
           params: {
-            user_id: userId,
+            user_id: store.userId,
           },
         },
       }));
-      const presenceChannel = store.pusher.subscribe('presence-chat');
-
-      // When a user subscribes
-      presenceChannel.bind('pusher:subscription_succeeded', (members: any) => {
-        console.log('Users online:', members.members);
-        members.each((member: any) => {
-          console.log(`User subscribed: ${ member.info.name }`);
-        });
-      });
-
-      // When a user joins
-      presenceChannel.bind('pusher:member_added', (member: any) => {
-        console.log(`${ member.info.name } joined`);
-      });
-
-      // When a user leaves
-      presenceChannel.bind('pusher:member_removed', (member: any) => {
-        console.log(`${ member.info.name } leaved`);
-      });
+      initPresenceChannel();
 
       // Subscribe me to my channel (listener)
       store.channel = noSerialize(store.pusher.subscribe(store.channelName));
@@ -191,6 +225,14 @@ export default component$(() => {
         store.artyom.say(data.message);
       });
     });
+  });
+
+  useOnDocument("qinit", $(() => {
+    setTimeout(async () => {
+      await artyomInit();
+    }, 3000);
+
+    initPusher();
   }));
 
   return (
@@ -205,14 +247,17 @@ export default component$(() => {
       <h3 class="py-4 text-center">{ store.receivedMessage }</h3>
 
       <div class="container flex flex-col gap-4">
-        <form preventdefault:submit onSubmit$={ sendMessage } class="flex flex-row w-full">
+        { store.onlineUsers.length > 0 && <Select options={ store.onlineUsers } onInput={ selectUser } label={ $localize`Choose a user` } placeholder={ $localize`Choose a user` } selected={ store.selectedUser } name="user" /> }
+
+        { store.selectedUser && <form preventdefault:submit onSubmit$={ sendMessage } class="flex flex-row w-full">
           <Input name="message" placeholder={ $localize`Write a message` } label={ $localize`Write a message or speak` } value={ store.message } onInput={ setMessage } />
           <BButton type="submit" class="primary" iconRight="send" loading={ store.sendingMessage } disabled={ !store.message }>{ $localize`Send` }</BButton>
-        </form>
-        <Select options={ store.supportedLangs } label={ $localize`Your language` } onInput={ changeLang } selected={ store.sourceLang } name="lang" />
-        <Select options={ store.voices } onInput={ changeVoice } label={ $localize`Choose a voice` } name="voice" />
+        </form> }
 
-        <Select options={ store.supportedLangs } label={ $localize`Target language` } onInput={ changeTargetLang } selected={ store.targetLang } name="target_lang" />
+        { store.supportedLangs.length > 0 && <Select options={ store.supportedLangs } label={ $localize`Your language` } placeholder={ $localize`Select your language` } onInput={ changeLang } selected={ store.sourceLang } name="lang" /> }
+
+        { store.supportedLangs.length > 0 && <Select options={ store.supportedLangs } label={ $localize`Target language` } placeholder={ $localize`Select target language` } onInput={ changeTargetLang } selected={ store.targetLang } name="target_lang" /> }
+        { store.voices.length > 0 && <Select options={ store.voices } onInput={ changeVoice } label={ $localize`Choose a voice` } name="voice" /> }
       </div>
     </div>
   );
