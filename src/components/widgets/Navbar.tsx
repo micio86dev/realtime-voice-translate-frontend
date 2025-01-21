c387e22b70d5f9da3c62bdea912f165981199027
Alessandro Micelli <micio86dev@gmail.com>
2025-01-21 11:25:19 +0000

    test 4

--- src/components/widgets/Navbar.tsx
+++ src/components/widgets/Navbar.tsx
@@ -1,14 +1,13 @@
 import {
   component$,
 } from '@builder.io/qwik';
-import ImgLogo from '~/assets/images/logo.png?jsx';
 
 export default component$(() => {
 
   return (
     <div class="navbar">
       <div class="logo">
-        <ImgLogo class="logo-img" />
+        <img class="logo-img" src="/images/logo.webp" alt="Logo" />
       </div>
       <h1>{ $localize`Realtime Voice Translate` }</h1>
     </div>
